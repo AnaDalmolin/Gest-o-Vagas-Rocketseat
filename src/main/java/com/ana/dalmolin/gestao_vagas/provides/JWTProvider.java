84192108ebd8392463563bb2bb509e8669ee67d6
AnaDalmolin <anadv21@hotmail.com>
2024-10-10 11:34:09 -0300

    2 Modulo Parte 02

---
+++ src/main/java/com/ana/dalmolin/gestao_vagas/provides/JWTProvider.java
@@ -0,0 +1,39 @@
+package com.ana.dalmolin.gestao_vagas.provides;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+@Service
+public class JWTProvider {
+    
+    @Value("${security.token.secret}")
+    private String secreteKey;
+
+
+    public String validateToken(String token){
+        token = token.replace("Bearer ", "");
+        Algorithm algorithm = Algorithm.HMAC256(secreteKey);
+       
+        try {
+            var subject = JWT.require(algorithm)
+            .build()
+            .verify(token)
+            .getSubject();
+            return subject;
+        } catch (JWTVerificationException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    // public String generateToken(String email){
+    //     return "token";
+    // }
+}
